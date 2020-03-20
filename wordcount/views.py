fa5b9523bc1c904f4d6352afbc6ea37cf081171b
Jerin <jerinthomas17@gamil.com>
2020-03-20 19:23:48 +0530

    wordcount Django

---
+++ wordcount/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+def homepage(request):
+	return render(request, 'home.html', {'hithere':'this is me'})
+
+def count(request):
+	fulltext = request.GET['fulltext']
+	wordlist = fulltext.split()
+	worddict = dict()
+
+	for word in wordlist:
+		if word in worddict:
+			# increase
+			worddict[word] += 1
+		else:
+			# add the word
+			worddict[word] = 1
+
+	sortedwords = sorted(worddict.items(), key = operator.itemgetter(1), reverse = True)
+
+	return render(request, 'count.html', {'fulltext' : fulltext, 'count': len(wordlist), 'worddict' : sortedwords, 'wordlist' : worddict.items() })
+
+def aboutpage(request):
+	return render(request, 'about.html')
